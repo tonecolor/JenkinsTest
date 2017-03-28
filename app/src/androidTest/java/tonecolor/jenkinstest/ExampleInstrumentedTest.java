91a4ba4cb347fb31d32863469b49f8243b73b841
ban <bankiki@naver.com>
2017-03-28 18:11:10 +0900

    테스트코드 삭제, 빌드 설정 변경

--- app/src/androidTest/java/tonecolor/jenkinstest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/tonecolor/jenkinstest/ExampleInstrumentedTest.java
@@ -1,3 +1,4 @@
+/*
 package tonecolor.jenkinstest;
 
 import android.content.Context;
@@ -9,11 +10,13 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+*/
 /**
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+ *//*
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -24,3 +27,4 @@ public class ExampleInstrumentedTest {
         assertEquals("tonecolor.jenkinstest", appContext.getPackageName());
     }
 }
+*/
